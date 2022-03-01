ba6220a0bec8498381c715d5aefeb56a707f446a
Жилкин Денис <zdv@2i.tusur.ru>
2022-03-01 18:03:15 +0700

    feat: parse all bitrix links in content, remove side bar

--- classes/Grabber.php
+++ classes/Grabber.php
@@ -5,6 +5,10 @@ declare(strict_types=1);
 namespace BitrixKB;
 
 use DOMDocument;
+use DOMComment;
+use DOMEntityReference;
+use DOMText;
+use DOMDocumentType;
 use Exception;
 
 class Grabber
@@ -74,7 +78,10 @@ class Grabber
         if (!file_exists($file)) {
             $content = file_get_contents($href);
             if ($content === false) {
-                throw new Exception(__FUNCTION__ . ': Can`t get source file content');
+                // throw new Exception(__FUNCTION__ . ': Can`t get source file content: ' . $href);
+                // pr(__FUNCTION__ . ': Can`t get source file content: ' . $href);
+                print(__FUNCTION__ . ': Can`t get source file content: ' . $href . PHP_EOL);
+                $content = '';
             }
             $content = $this->preprocessContent($content);
             file_put_contents($file, $content);
@@ -84,22 +91,28 @@ class Grabber
     # save 
     protected function getSource(string $href): string
     {
-        $path = pathinfo($href);
+        if ($clean = strstr($href, '?', true)) {
+            $path = pathinfo($clean);
+        } else {
+            $path = pathinfo($href);
+        }
         $dir = $path['dirname'];
         $dir = str_replace(['/bitrix/', 'https://'], '', $dir);
         if (key_exists('extension', $path)) {
-            $clean = strstr($path['extension'], '?', true);
-            $ext = $clean ? $clean : $path['extension'];
-            $file = $path['filename'] . '.' . $ext;
+            if  ($path['extension'] == 'php') {
+                return $href;
+            }
+            $file = $path['filename'] . '.' . $path['extension'];
         } else {
-            $clean = strstr($path['filename'], '?', true);
-            $file = $clean ? $clean : $path['filename'];
+            $file = $path['filename'];
         }
-
-        print($href) . PHP_EOL;
-        print($dir)  . PHP_EOL;
-        print($file) . PHP_EOL;
-
+        /*
+        print(PHP_EOL);
+        print($href . PHP_EOL);
+        print($dir  . PHP_EOL);
+        print($file . PHP_EOL);
+        print(PHP_EOL);
+        /**/
         if (ord($href) == 47) { # 47 is /
             $href = $this->root . $href;
         }
@@ -116,10 +129,13 @@ class Grabber
     {
         # extract links
         $links = [];
-        if (!preg_match_all("#'[^\s()<>,]+'#", $content, $links)) {
+        // (https:\/\/|\/bitrix\/)[^\s()<>,'\"]+ too much
+        // (https:\/\/bitrix|\/bitrix\/)[^\s()<>,'\"]+
+        if (preg_match_all("/(https:\/\/bitrix|\/bitrix\/)[^\s()<>,'\"]+/", $content, $links) === false) {
             throw new Exception('Can`t extract url from content');
         }
         $links = $links[0];
+        // print_r($links);
         # get and save file, rewrite links
         foreach ($links as $link) {
             $localLink = $this->getSource($link);
@@ -127,6 +143,31 @@ class Grabber
         }
 
         return $content;
+    }
+
+    protected function rewriteUrls($nodeList): void
+    {
+        foreach ($nodeList as $node) {
+            if ($node instanceof DOMDocumentType || $node instanceof DOMText || $node instanceof DOMComment || $node instanceof DOMEntityReference || $node->nodeName == 'a') {
+                continue;
+            }
+            if ($href = $node->getAttribute('href')) {
+                $localUrl = $this->getSource($href);
+                $node->setAttribute('href', $localUrl);
+            } elseif ($src = $node->getAttribute('src')) {
+                $localUrl = $this->getSource($src);
+                $node->setAttribute('src', $localUrl);
+            } elseif (in_array($node->nodeName, ['style', 'script'])) {
+                $node->nodeValue = $this->preprocessContent($node->nodeValue);
+            }
+            if ($style = $node->getAttribute('style')) {
+                $style = $this->preprocessContent($style);
+                $node->setAttribute('style', $style);
+            }
+            if ($node->hasChildNodes()) {
+                $this->rewriteUrls($node->childNodes);
+            }
+        }
     }
 
     public function getPage(string $location, string $pageName): void
@@ -138,27 +179,17 @@ class Grabber
         # Убираем шапку и подвал от Битрикса
         $body = $dom->getElementsByTagName('body')->item(0);
         foreach ($body->getElementsByTagName('div') as $div) {
-            if (in_array($div->getAttribute('class'), ['landing-pub-top-panel-wrapper', 'bitrix-footer'])) {
-                if ($body->removeChild($div) === false) {
+            $classes = $div->getAttribute('class');
+            $classes = explode(' ', $classes);
+            if ($a = array_intersect($classes, ['landing-pub-top-panel-wrapper', 'bitrix-footer', 'landing-sidebar'])) {
+                if ($div->parentNode->removeChild($div) === false) {
                     throw new Exception('Removing failed');
                 }
             }
         }
 
-        foreach ($dom->getElementsByTagName('link') as $link) {     
-            $href = $link->getAttribute('href');
-            $localRef = $this->getSource($href);
-            $link->setAttribute('href', $localRef);
-        }
-
-        foreach ($dom->getElementsByTagName('script') as $script) {
-            if ($src = $script->getAttribute('src')) {
-                $localSrc = $this->getSource($src);
-                $script->setAttribute('src', $localSrc);
-            } else {
-                $script->nodeValue = $this->preprocessContent($script->nodeValue);
-            }
-        }
+        # Сохраняем локально контент и заменяем ссылки на него в html
+        $this->rewriteUrls($dom->childNodes);
 
         $file = $this->destinationFolder . DIRECTORY_SEPARATOR . $pageName . '.html';
         if (!$dom->saveHTMLFile($file)) {
